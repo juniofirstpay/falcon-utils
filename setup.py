434e3d55a522eaca33054799753088b26dc380ea
Nirbhay Gupta <guptanirbhay93@gmail.com>
2022-09-09 08:42:54 +0530

    [Version] Upgrade

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         'falcon_utils.routes',
         'falcon_utils.hooks'
     ],
-    version='0.0.9',
+    version='0.0.10',
     author="Develper Junio",
     author_email='developer@junio.in',
     classifiers=[
