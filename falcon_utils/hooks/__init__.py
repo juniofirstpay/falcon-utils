748142a13dbeb63e9d9d82e014f3a78f8ac31e02
Nirbhay Gupta <guptanirbhay93@gmail.com>
2022-10-22 00:05:45 +0530

    [Hooks] Mongo models injects

--- falcon_utils/hooks/__init__.py
+++ falcon_utils/hooks/__init__.py
@@ -1,3 +1,4 @@
 from .serialize_schema import SerializeSchema
 from .validate_schema import ValidateParams, ValidateSchema
 from .map_query import MapQuery
+import mongo
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         'falcon_utils.hooks',
         'falcon_utils.response'
     ],
-    version='0.0.25',
+    version='0.0.26',
     author="Develper Junio",
     author_email='developer@junio.in',
     classifiers=[
