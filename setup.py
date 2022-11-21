bdcd01a20aa7f44d7badcb5bb3d02a0616816c4f
Nirbhay Gupta <guptanirbhay93@gmail.com>
2022-11-21 18:16:56 +0530

    [middlewares][auth]options request handling

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         'falcon_utils.hooks.mongo',
         'falcon_utils.response'
     ],
-    version='0.0.28',
+    version='0.0.29',
     author="Develper Junio",
     author_email='developer@junio.in',
     classifiers=[
